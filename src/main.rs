18935e3fed0c933b3c4bfdd292a0b6c8b5520d4a
JAStrachan <jstrachansss@gmail.com>
2020-08-03 15:42:55 +0100

    First replacement of word in sentences

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+use regex::Regex;
+
 fn main() {
-    println!("{}", separate_sentence(String::from("Sentence to split")));
+    println!("{}", separate_sentence(String::from("Sentence to @noun")));
 }
 
 fn separate_sentence(sentence: String) -> String {
-    let split_sentence: Vec<&str> = sentence.split_whitespace().collect();
-    fill_word();
-    split_sentence.into_iter().collect()
+    let mut new_sentence = sentence.clone();
+
+    for mat in Regex::new(r"@\w*").unwrap().find_iter(&sentence) {
+        let word = &sentence[mat.start()..mat.end()];
+        let new_word = fill_word(word);
+        new_sentence.replace_range(mat.range(), new_word)
+    }
+
+    new_sentence
 }
 
-fn fill_word() -> &'static str {
-    "Hello"
+fn fill_word(type_of_word: &str) -> &'static str {
+    match type_of_word {
+        "@noun" => "hospital",
+        _ => "Not a templated character",
+    }
 }
