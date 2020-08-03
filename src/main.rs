a80685537ff6f4c782bae01bb03159d782ef39b2
JAStrachan <jstrachansss@gmail.com>
2020-08-03 13:32:43 +0100

    splitting up sentence on whitespace

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("{}", fill_word());
+    println!("{}", separate_sentence(String::from("Sentence to split")));
+}
+
+fn separate_sentence(sentence: String) -> String {
+    let split_sentence: Vec<&str> = sentence.split_whitespace().collect();
+    fill_word();
+    let new_sentence: String = split_sentence.into_iter().collect();
+    new_sentence
 }
 
 fn fill_word() -> &'static str {
