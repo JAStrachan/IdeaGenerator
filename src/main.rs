c79d5c9c5ef334d14e3f5e93faaf90439887e88b
JAStrachan <jstrachansss@gmail.com>
2020-10-03 21:28:33 +0100

    replaces multiple symbols in a input

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,45 @@ fn main() {
     println!(
         "{}",
         separate_sentence(String::from(
-            "Help I need to run to that , @verb, , @building, help"
+            "Help I need to run to that , @verb, , @building, help goodness me I need another @verb to test out"
         ))
     );
 }
 
-// Doesn't work on multiple tokens because it doesn't relevaluate where the next token is. Do I have to build a AST?????
+// Does work for two  + possi more @symbols but doesn't get the last part of the sentence after the last symbol
 fn separate_sentence(sentence: String) -> String {
-    let mut new_sentence = sentence.clone();
+    let mut ast: Vec<&str> = Vec::new();
+    let mut last_index = 0;
 
     for mat in Regex::new(r"@\w*").unwrap().find_iter(&sentence) {
-        let word = &sentence[mat.start()..mat.end()];
-        let new_word = fill_word(word);
-
-        new_sentence.replace_range(mat.range(), &new_word);
+        ast.push(&sentence[last_index..mat.start()]);
+        ast.push(&sentence[mat.start()..mat.end()]);
+        last_index = mat.end();
     }
+    ast.push(&sentence[last_index..sentence.len()]);
 
-    new_sentence
+    let new_sentence_ast: Vec<String> = ast.into_iter().map(|word| fill_word(word)).collect();
+    new_sentence_ast.into_iter().map(|i| i).collect::<String>()
 }
 
 fn fill_word(type_of_word: &str) -> String {
     match type_of_word {
         "@building" => random_word("buildings.txt"),
         "@verb" => random_word("verbs.txt"),
-        _ => String::from("Not a templated character"),
+        _ => String::from(type_of_word),
     }
 }
 
 fn random_word(filename: &str) -> String {
     let contents = read_file(filename).expect("Problem with reading the file");
 
-    let mut words = Vec::new();
-    for line in contents.lines() {
-        words.push(line);
-    }
-
-    choose_random_word(words)
+    String::from(choose_random_word(contents.lines().collect()))
 }
 
-fn choose_random_word(list_of_words: Vec<&str>) -> String {
+fn choose_random_word(list_of_words: Vec<&str>) -> &str {
     let mut rng = rand::thread_rng();
     let random_number = rng.gen_range(0, list_of_words.len());
-    return String::from(list_of_words[random_number]);
+    return list_of_words[random_number];
 }
 
 fn read_file(filename: &str) -> Result<(String), Box<dyn Error>> {
